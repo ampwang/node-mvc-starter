5fe58e69234b09bceaf672b71daa017ba429450f
ampwang <wichanart@gmail.com>
2021-11-27 22:54:04 +0700

    Remove unused code

--- routers/main.js
+++ routers/main.js
@@ -1,17 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const users = [];
-
 router.get('/', (req, res) => {
   res.render('main', { pageTitle: 'Main'});
 });
 
-router.post('/', (req, res) => {
-  users.push(req.body.name);
-  console.log(users);
-  // res.redirect('/users');
-});
-
 exports.routes = router;
-exports.users = users;
